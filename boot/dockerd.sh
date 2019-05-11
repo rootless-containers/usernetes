6987ac5984841662a10be6aa794e2d1cf9487d00
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2019-05-11 16:23:13 +0900

    update components
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- boot/dockerd.sh
+++ boot/dockerd.sh
@@ -5,5 +5,4 @@ source $U7S_BASE_DIR/common/common.inc.sh
 exec $(dirname $0)/nsenter.sh dockerd \
 	--experimental \
 	--storage-driver $(overlayfs::supported && echo overlay2 || echo vfs) \
-	--userland-proxy --userland-proxy-path $(which rootlesskit-docker-proxy) \
 	$@
