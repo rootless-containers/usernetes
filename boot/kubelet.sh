2960d4e17ba0bae1c174511a6d2f5f0a91af2be3
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2020-04-29 14:37:24 +0900

    Fedora 32
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- boot/kubelet.sh
+++ boot/kubelet.sh
@@ -11,10 +11,11 @@ exec $(dirname $0)/nsenter.sh kubelet \
 	--anonymous-auth=true \
 	--authorization-mode=AlwaysAllow \
 	--fail-swap-on=false \
-	--feature-gates DevicePlugins=false,SupportNoneCgroupDriver=true \
+	--feature-gates DevicePlugins=false,SupportNoneCgroupDriver=true,LocalStorageCapacityIsolation=false \
 	--eviction-hard "nodefs.available<3%" \
 	--cgroup-driver=none --cgroups-per-qos=false --enforce-node-allocatable="" \
 	$@
 
 # Notes
 # --evictrion-hard: Relax disk pressure taint for CI
+# LocalStorageCapacityIsolation=false: workaround for "Failed to start ContainerManager failed to get rootfs info" error on Fedora 32: https://github.com/rootless-containers/usernetes/pull/157#issuecomment-621008594
