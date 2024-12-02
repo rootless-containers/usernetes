e4a550375c4acc99322d5d673bfab8d89ebda94f
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2024-12-02 19:44:43 +0900

    init-host: install Podman from Canonical
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- init-host/init-host.root.d/install-podman.sh
+++ init-host/init-host.root.d/install-podman.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# This script installs the latest release of Podman.
-# Repository information is from https://podman.io/docs/installation#linux-distributions
+# This script installs Podman.
 set -eux -o pipefail
 if [ "$(id -u)" != "0" ]; then
 	echo "Must run as the root"
@@ -10,16 +9,6 @@ fi
 if command -v dnf >/dev/null 2>&1; then
 	dnf install -y podman podman-compose
 else
-	mkdir -p /etc/apt/keyrings
-	curl -fsSL "https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_$(lsb_release -rs)/Release.key" |
-		gpg --dearmor |
-		tee /etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg >/dev/null
-	echo \
-		"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg]\
-        https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_$(lsb_release -rs)/ /" |
-		tee /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list >/dev/null
 	apt-get update -qq
-	apt-get -qq -y install podman
-	# No dpkg for podman-compose ?
-	pip3 install podman-compose
+	apt-get -qq -y install podman podman-compose
 fi
