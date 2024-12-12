21331b31e10e2f0e47b00cdceab2e0663ca1e54d
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2024-12-12 12:29:54 +0900

    Consistently remove "U7S_" prefix from all the variables
    Fix issue 356
    
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- Makefile.d/install-flannel.sh
+++ Makefile.d/install-flannel.sh
@@ -3,10 +3,10 @@ set -eu -o pipefail
 
 # See chart values, 0 indicates default for platform
 # https://github.com/flannel-io/flannel/blob/v0.26.1/chart/kube-flannel/values.yaml
-: "${U7S_PORT_FLANNEL:='0'}"
+: "${PORT_FLANNEL:='0'}"
 
 if ! helm -n kube-flannel list -q | grep flannel; then
 	kubectl create namespace kube-flannel
 	kubectl label --overwrite namespace kube-flannel pod-security.kubernetes.io/enforce=privileged
-	helm install flannel --namespace kube-flannel --set-json flannel.backendPort=${U7S_PORT_FLANNEL} /flannel
+	helm install flannel --namespace kube-flannel --set-json flannel.backendPort=${PORT_FLANNEL} /flannel
 fi
